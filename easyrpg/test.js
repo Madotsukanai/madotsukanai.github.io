29b060bb7a5fda19e650e27e2e068194ebe32f04
Madotsukanai <tdn25chu@gmail.com>
2026-07-06 15:48:47 +0900

    test

--- easyrpg/test.js
+++
@@ -1,3 +0,0 @@
-const value = "Soundfont=/Soundfont/Scc1t2.sf2";
-const soundfont = value.split('/').pop() || '';
-console.log(soundfont);
--- easyrpg/test_ini.js
+++
@@ -1,2 +0,0 @@
-const fs = require('fs');
-console.log("We don't have a specific RPG_RT.ini handy");
